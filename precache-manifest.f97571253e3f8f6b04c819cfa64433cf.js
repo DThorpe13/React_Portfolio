913a69483afa815218eefc7b75b1d62d5c7b1ee6
Derek Thorpe <derekthorpe13@gmail.com>
2021-01-24 23:59:19 -0500

    Updates

---
+++ precache-manifest.f97571253e3f8f6b04c819cfa64433cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5cced8afdf2639c7cd7c",
+    "url": "/React_Portfolio/static/css/main.ea9faaff.chunk.css"
+  },
+  {
+    "revision": "5cced8afdf2639c7cd7c",
+    "url": "/React_Portfolio/static/js/main.5cced8af.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/React_Portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "0b3b1d1736f2c5113254",
+    "url": "/React_Portfolio/static/js/runtime~main.0b3b1d17.js"
+  },
+  {
+    "revision": "8657675460890dbf92861440c412dfd0",
+    "url": "/React_Portfolio/index.html"
+  }
+];
